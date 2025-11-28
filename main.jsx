d1f95c80d750aed7fc8c2d1810177f025a75abed
alan alcantara <alan84515@gmail.com>
2025-11-28 15:35:15 -0300

    insights

--- main.jsx
+++ main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import './index.css'
 
 createRoot(document.getElementById('root')).render(<App />)
